5692e16ed04172e4763e3dec4c178d885d705bd5
Nicola Piga <nicolapiga@gmail.com>
2019-11-22 18:01:14 +0100

    Support Python >= 3.6

--- samples/humanoids_pouring/kickstart_script_bottles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ../../../virtualenvs/mask-rcnn/bin/activate
-
-python online_demo_bottles.py --width 320 --height 240 bottles_humanoids_training/mask_rcnn_ycb_video_training_0010.h5
